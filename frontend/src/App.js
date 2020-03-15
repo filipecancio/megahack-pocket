0b01f2f581d66f0f06c94d7825f8e8f22e1f6eef
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-15 00:49:52 -0300

    ➕ Add axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+  const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept"
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      axios
+        .get(
+          `https://us-central1-megapoc.cloudfunctions.net/api/inicio`,
+          config
+        )
+        .then(res => {
+          const response = res.data;
+          console.log(response);
+          setData(response);
+        });
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Hello world</h1>
