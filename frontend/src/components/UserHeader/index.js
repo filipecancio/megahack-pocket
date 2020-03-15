cf5ffec758b2e1e8341ffb09de64a40695fa3f06
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-15 19:32:42 -0300

    🚧 update component: UserHeader

--- frontend/src/components/UserHeader/index.js
+++ frontend/src/components/UserHeader/index.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { Component } from "./styles";
+import { Container } from "./styles";
 
 function UserHead(props) {
   return (
     <>
-      <span>Logo</span>
-      <div>
-        <p>Início</p>
-        <p>Statups</p>
-        <p>Comunidades</p>
-        <p>Programas</p>
-        <p>Estatíticas</p>
-      </div>
+      <Container />
     </>
   );
 }
--- frontend/src/components/UserHeader/styles.js
+++ frontend/src/components/UserHeader/styles.js
@@ -1,3 +1,8 @@
 import styled from "styled-components";
+import logo from "../../icons/default_logo.png";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 238px;
+  background: url(${logo}) center no-repeat;
+`;
