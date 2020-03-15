14190ab3d10a8c6fc51f61fdcca59cb82b7b4c37
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-15 20:51:00 -0300

    🚧Update component: BodyContent

--- frontend/src/components/BodyContent/index.js
+++ frontend/src/components/BodyContent/index.js
@@ -1,10 +1,48 @@
 import React from "react";
 import { Container } from "./styles";
+import CustomPieChart from "../PieChart";
+import CustomAreaChart from "../AreaChart";
+import data from "./data.json";
 
 function BodyContent(props) {
   return (
     <>
-      <Container></Container>
+      <Container>
+        <div>
+          <h1>Cidades</h1>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+          <p>Anchieta</p>
+        </div>
+        <div>
+          <CustomPieChart data={data.chart01.data} title={data.chart01.title} />
+          <div>
+            <CustomPieChart
+              data={data.chart01.data}
+              title={data.chart01.title}
+            />
+            <CustomPieChart
+              data={data.chart01.data}
+              title={data.chart01.title}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1>Estado</h1>
+            <p>Espirito Santo</p>
+          </div>
+          <div>
+            <h1>Total de Empresas</h1>
+            <p>12.932</p>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
--- frontend/src/components/BodyContent/styles.js
+++ frontend/src/components/BodyContent/styles.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 100%;
-  height: 600px;
+  min-height: 600px;
+  height: 100%;
+  padding: 50px;
   background-color: #e6e9ec;
+  display: flex;
 `;
