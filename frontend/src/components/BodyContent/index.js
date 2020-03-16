f9f00e0a91d2ac7ae9cb7284dc9069cebe10de1d
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-15 22:59:13 -0300

    🚧Organize charts

--- frontend/src/components/BodyContent/index.js
+++ frontend/src/components/BodyContent/index.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { Container , ContainerGraph1, SideRight , SideLeft, 
-  ContainerGraph, TableLeft} from "./styles";
+import {
+  Container,
+  ContainerGraph1,
+  SideRight,
+  SideLeft,
+  ContainerGraph,
+  TableLeft
+} from "./styles";
 
 import CustomPieChart from "../PieChart";
 import CustomAreaChart from "../AreaChart";
 
 function BodyContent(props) {
   return (
-  <>
+    <>
       <Container>
         <TableLeft>
-            <h1>Cidades</h1>
-            {props.data.citys.map(element => (
-              <p>{element}</p>
-            ))}
+          <h1>Cidades</h1>
+          {props.data.citys.map(element => (
+            <p>{element}</p>
+          ))}
         </TableLeft>
         <ContainerGraph>
           <ContainerGraph1>
-            <CustomPieChart 
-              data={props.data.chart01.data} 
+            <CustomPieChart
+              data={props.data.chart01.data}
               title={props.data.chart01.title}
-              />
+            />
           </ContainerGraph1>
-          
+
           <div>
             <CustomAreaChart
               data={props.data.chart02.data}
@@ -32,21 +38,23 @@ function BodyContent(props) {
             <CustomPieChart
               data={props.data.chart03.data}
               title={props.data.chart03.title}
-              innerRadius = {60}/>
+              innerRadius={60}
+            />
           </div>
-        </ContainerGraph>        
+        </ContainerGraph>
         <SideRight>
           <div>
             <h1>Estado</h1>
             <p>Espirito Santo</p>
           </div>
           <div>
-            <h1>Total de Empresas</h1>
+            <h1>Statups</h1>
+            <h3>Total</h3>
             <p>12.932</p>
           </div>
         </SideRight>
       </Container>
-  </>
+    </>
   );
 }
 
